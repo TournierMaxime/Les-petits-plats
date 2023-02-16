391f9a998042b44292a5b759f1d2f3a619b2f457
Hoggy06 <speedrun.fra@gmail.com>
2023-02-16 17:49:17 +0100

    fix oriented object

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,83 @@
-//Renseigne la fonction creaListeDom pour les paramètres
-function displayList(recipe) {
-  creaListeFiltre(recipe)
-  creaListeDom(tabIngredients, "ingredients")
-  creaListeDom(tabAppareils, "devices")
-  creaListeDom(tabUstensiles, "utensils")
+class App {
+  constructor(recipes) {
+    this.recipes = recipes
+    this.cartesRecettes = document.querySelector(".recipesCards")
+    this.tabIngredients = []
+    this.tabUstensiles = []
+    this.tabAppareils = []
+  }
+
+  displayRecette(recipes) {
+    this.cartesRecettes.innerHTML = ""
+
+    recipes.forEach(({ ...recette }) => {
+      const creaData = new Recipe(
+        recette.id,
+        recette.name,
+        recette.servings,
+        recette.ingredients,
+        recette.time,
+        recette.description,
+        recette.appliance,
+        recette.ustensils
+      )
+      const creaCarte = creaData.createCardRecipe()
+      this.cartesRecettes.appendChild(creaCarte)
+    })
+  }
+
+  noResultsFound() {
+    this.cartesRecettes.innerHTML = ""
+
+    const divnull = document.createElement("div")
+    divnull.id = "pas_de_recette"
+    divnull.textContent =
+      'Aucune recette ne correspond à vos critères, veuillez modifier votre recherche svp. Vous pouvez chercher "tarte aux pommes", "poisson", etc...'
+    this.cartesRecettes.appendChild(divnull)
+  }
+
+  displayList(recipe) {
+    this.creaListeFiltre(recipe)
+    this.creaListeDom(this.tabIngredients, "ingredients")
+    this.creaListeDom(this.tabAppareils, "devices")
+    this.creaListeDom(this.tabUstensiles, "utensils")
+  }
+
+  creaListeFiltre(recipe) {
+    recipe.forEach(({ ingredients, ustensils, appliance }) => {
+      ingredients.forEach(({ ingredient }) => {
+        if (!this.tabIngredients.includes(ingredient)) {
+          this.tabIngredients.push(ingredient)
+        }
+      })
+      ustensils.forEach((ustensil) => {
+        if (!this.tabUstensiles.includes(ustensil)) {
+          this.tabUstensiles.push(ustensil)
+        }
+      })
+      if (!this.tabAppareils.includes(appliance)) {
+        this.tabAppareils.push(appliance)
+      }
+    })
+  }
+
+  creaListeDom(tab, type) {
+    const list = document.getElementById(type + "_div")
+    list.innerHTML = ""
+
+    const ul = document.createElement("ul")
+    ul.type = type
+
+    list.appendChild(ul)
+
+    tab.forEach((item) => {
+      const li = document.createElement("li")
+      li.className = "li_" + type
+      li.innerHTML = item
+      ul.appendChild(li)
+    })
+  }
 }
 
-const cartesRecettes = document.querySelector(".recipesCards")
-
-//Message en cas de recette non trouvée
-function noResultsFound() {
-  cartesRecettes.innerHTML = ""
-
-  const divnull = document.createElement("div")
-  divnull.id = "pas_de_recette"
-  divnull.textContent =
-    'Aucune recette ne correspond à vos critères, veuillez modifier votre recherche svp. Vous pouvez chercher "tarte aux pommes", "poisson", etc...'
-  cartesRecettes.appendChild(divnull)
-}
-
-function displayRecette(recipes) {
-  cartesRecettes.innerHTML = ""
-
-  recipes.forEach(({ ...recette }) => {
-    const creaData = new Recipe(
-      recette.id,
-      recette.name,
-      recette.servings,
-      recette.ingredients,
-      recette.time,
-      recette.description,
-      recette.appliance,
-      recette.ustensils
-    )
-    const creaCarte = creaData.createCardRecipe()
-    cartesRecettes.appendChild(creaCarte)
-  })
-}
-
-function init() {
-  displayRecette(recipes)
-  displayList(recipes)
-}
-
-init()
+const app = new App(recipes)
+app.displayRecette(recipes)
--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,77 +1,86 @@
-const searchBar = document.getElementById("searchBar")
-// Méthode auxiliaire pour vérifier si une chaîne de caractères inclut une sous-chaîne donnée, sans tenir compte de la casse
-const includesIgnoreCase = (str, subStr) => {
-  return str.toLowerCase().includes(subStr.toLowerCase())
-}
-
-// Méthode pour effectuer une recherche sur les recettes
-const search = (query) => {
-  // Chercher les recettes qui correspondent à la requête
-  const matches = []
-
-  // Convertir la requête en minuscules
-  query = query.toLowerCase()
-
-  // Vérifier que la requête est suffisamment longue
-  if (query.length < 3) {
-    return []
+class SearchEngine {
+  constructor(recipes) {
+    this.recipes = recipes
   }
 
-  // Afficher un message d'erreur si aucun résultat n'est trouvé
-  if (matches.length === 0) {
-    noResultsFound()
+  // Méthode auxiliaire pour vérifier si une chaîne de caractères inclut une sous-chaîne donnée, sans tenir compte de la casse
+  includesIgnoreCase = (str, subStr) => {
+    return str.toLowerCase().includes(subStr.toLowerCase())
   }
 
-  for (const recipe of recipes) {
-    // Vérifier si le nom de la recette inclut la requête
-    if (includesIgnoreCase(recipe.name, query)) {
-      matches.push(recipe)
-    } else {
-      // Vérifier si un ingrédient de la recette inclut la requête
-      for (const ingredient of recipe.ingredients) {
-        if (includesIgnoreCase(ingredient.ingredient, query)) {
+  // Méthode pour effectuer une recherche sur les recettes
+  search = (query) => {
+    // Chercher les recettes qui correspondent à la requête
+    const matches = []
+
+    // Convertir la requête en minuscules
+    query = query.toLowerCase()
+
+    // Vérifier que la requête est suffisamment longue
+    if (query.length < 3) {
+      return []
+    }
+
+    // Afficher un message d'erreur si aucun résultat n'est trouvé
+    if (matches.length === 0) {
+      app.noResultsFound()
+    }
+
+    for (const recipe of recipes) {
+      // Vérifier si le nom de la recette inclut la requête
+      if (this.includesIgnoreCase(recipe.name, query)) {
+        matches.push(recipe)
+      } else {
+        // Vérifier si un ingrédient de la recette inclut la requête
+        for (const ingredient of recipe.ingredients) {
+          if (this.includesIgnoreCase(ingredient.ingredient, query)) {
+            matches.push(recipe)
+            break
+          }
+        }
+      }
+
+      // Vérifier si un ustensile de la recette inclut la requête
+      for (const ustensil of recipe.ustensils) {
+        if (this.includesIgnoreCase(ustensil, query)) {
           matches.push(recipe)
           break
         }
       }
-    }
 
-    // Vérifier si un ustensile de la recette inclut la requête
-    for (const ustensil of recipe.ustensils) {
-      if (includesIgnoreCase(ustensil, query)) {
+      // Vérifier si l'appareil de la recette inclut la requête
+      if (this.includesIgnoreCase(recipe.appliance, query)) {
         matches.push(recipe)
-        break
       }
     }
 
-    // Vérifier si l'appareil de la recette inclut la requête
-    if (includesIgnoreCase(recipe.appliance, query)) {
-      matches.push(recipe)
+    // Afficher un message d'erreur si aucun résultat n'est trouvé
+    if (matches.length === 0) {
+      app.noResultsFound()
+    } else {
+      const uniqueMatches = [...new Set(matches)]
+      app.displayRecette(uniqueMatches)
+      app.displayList(uniqueMatches)
+
+      // Retourner les recettes correspondant à la requête, sans doublons
+      return uniqueMatches
     }
   }
-
-  // Afficher un message d'erreur si aucun résultat n'est trouvé
-  if (matches.length === 0) {
-    noResultsFound()
-  } else {
-    const uniqueMatches = [...new Set(matches)]
-    displayRecette(uniqueMatches)
-    displayList(uniqueMatches)
-
-    // Retourner les recettes correspondant à la requête, sans doublons
-    return uniqueMatches
-  }
 }
+
+const searchEngine = new SearchEngine(recipes)
+
+const searchBar = document.getElementById("searchBar")
 
 searchBar.addEventListener("input", () => {
   const query = searchBar.value.trim()
   if (query.length < 3) {
     // Restaurer le contenu initial si la requête est trop courte
-    displayRecette(recipes)
+    app.displayRecette(recipes)
   } else {
-    const result = search(query)
+    const result = searchEngine.search(query)
     if (result && result.length === 0) {
-      noResultsFound()
+      app.noResultsFound()
     }
   }
 })
